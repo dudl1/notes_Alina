e5a9f1f9c91aa26c62b6157b61aa846cec33dc03
Valery <80582147+dudl1@users.noreply.github.com>
2026-02-01 02:04:44 +0700

    Add files via upload

---
+++ _app/immutable/chunks/BtS6hOu-.js
@@ -0,0 +1,1 @@
+import{o as a}from"./DNtSXVTb.js";a();
---
+++ _app/immutable/nodes/0.DGfeo9oR.js
@@ -0,0 +1,19 @@
+import{c as l,a as i,w as f,f as c}from"../chunks/DmGirFel.js";import"../chunks/BtS6hOu-.js";import{h as m,z as u,q as d,A as p}from"../chunks/DNtSXVTb.js";import{h}from"../chunks/D3xK0UoA.js";function _(n,e,s,a,r){m&&u();var t=e.$$slots?.[s],o=!1;t===!0&&(t=e.children,o=!0),t===void 0||t(n,o?()=>a:a)}const y=!0,v=!1,k=Object.freeze(Object.defineProperty({__proto__:null,prerender:y,ssr:v},Symbol.toStringTag,{value:"Module"}));var b=f(c(`<script src="https://cdn.tailwindcss.com"><\/script> <script>
+    tailwind.config = {
+      theme: {
+        extend: {
+          animation: {
+            'float': 'float 3s ease-in-out infinite',
+            'pulse-slow': 'pulse 3s ease-in-out infinite',
+            'bounce-slow': 'bounce 2s infinite',
+          },
+          keyframes: {
+            float: {
+              '0%, 100%': { transform: 'translateY(0)' },
+              '50%': { transform: 'translateY(-10px)' },
+            }
+          }
+        }
+      }
+    }
+  <\/script> <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"/>`,1));function q(n,e){var s=l();h("12qhfyh",r=>{var t=b();p(4),i(r,t)});var a=d(s);_(a,e,"default",{}),i(n,s)}export{q as component,k as universal};
